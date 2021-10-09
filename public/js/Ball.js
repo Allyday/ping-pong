5526c93ae25b8cd09ea2bb0f652c2f4144888e70
Hien-Anh (Ally) Tran <h.anhtr15@gmail.com>
2021-10-09 12:48:48 +0700

    move collision & movement logics into nodes instead of app

--- public/js/Ball.js
+++ public/js/Ball.js
@@ -1,8 +1,29 @@
 import { CanvasNode } from './CanvasNode.js';
 
 export class Ball extends CanvasNode {
-  constructor(id, canvas, sizeRatio, color) {
-    super(id, canvas.width / 2, canvas.height / 2, canvas.width * sizeRatio, canvas.width * sizeRatio, color);
+  constructor(id, canvas, sizeRatio, color, maxBounceAngle, velocity) {
+    super(
+      id,
+      canvas.width / 2,
+      canvas.height / 2,
+      canvas.width * sizeRatio,
+      canvas.width * sizeRatio,
+      color,
+      velocity,
+    );
     this.angle = Math.random() * Math.PI / 2 - Math.PI / 4; // 90 degree range, turned to the right (towards p2)
+    this.maxBounceAngle = maxBounceAngle;
+  }
+
+  bouncesFrom(node) {
+    /* bounceIntensity = how far the collision is from the center */
+    const relativeHeightDiff = (node.y + (node.height / 2)) - this.y;
+    const bounceIntensity = relativeHeightDiff / (node.height / 2);
+    const bounceAngle = bounceIntensity * this.maxBounceAngle;
+    /**
+     * deflect ball to the left if it was going right and vice versa
+     * only applies for collision with vertical nodes
+     */
+    this.angle = Math.abs(this.angle) > Math.PI / 2 ? bounceAngle : Math.PI - bounceAngle;
   }
 };
--- public/js/CanvasNode.js
+++ public/js/CanvasNode.js
@@ -1,10 +1,56 @@
 export class CanvasNode {
-  constructor (id, x, y, width, height, color) {
+  constructor(id, x, y, width, height, color, velocity) {
     this.id = id;
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
     this.color = color;
+    this.velocity = velocity;
+  }
+
+  #isWithinWidthsOf(node) {
+    return (this.x + this.width >= node.x) && (this.x <= node.x + node.width);
+  }
+
+  #isWithinHeightsOf(node) {
+    return (this.y + this.height >= node.y) && (this.y <= node.y + node.height);
+  }
+
+  collidesWith(node) {
+    return this.#isWithinWidthsOf(node) && this.#isWithinHeightsOf(node);
+  }
+
+  hitsCeiling() {
+    return this.y <= 0;
+  }
+
+  hitsFloor(canvas) {
+    return this.y + this.height >= canvas.height;
+  }
+
+  accelerate(a = 1.1) {
+    this.velocity *= a;
+  }
+
+  moves(vector, limitNodes) {
+    if (!limitNodes) {
+      this.x += vector.x * this.velocity;
+      this.y += vector.y * this.velocity;
+    } else {
+      const { leftLimit, rightLimit } = limitNodes;
+      
+      let newX;
+      const calculatedNewX = this.x + vector.x * this.velocity;
+      /* prevent ball & paddle overlap when collide */
+      if (vector.x < 0 && this.#isWithinHeightsOf(leftLimit))
+        newX = Math.max(calculatedNewX, leftLimit.x + leftLimit.width);
+      else if (vector.x > 0 && this.#isWithinHeightsOf(rightLimit))
+        newX = Math.min(calculatedNewX, rightLimit.x - this.width);
+      else newX = calculatedNewX;
+
+      this.x = newX;
+      this.y += vector.y * this.velocity;
+    }
   }
 };
